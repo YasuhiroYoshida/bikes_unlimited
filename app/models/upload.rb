9dc9c89aafa196526b7039161927068a0846e61a
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-10-30 12:31:44 +0300

    Add User
    All the license applicants in the uploaded csv files are called User,
    and they will be stored in users table.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,4 @@
 class Upload < ApplicationRecord
   has_one_attached :csv
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_085034) do
+ActiveRecord::Schema.define(version: 2021_10_30_092745) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2021_10_30_085034) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.integer "upload_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["upload_id"], name: "index_users_on_upload_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "users", "uploads"
 end
