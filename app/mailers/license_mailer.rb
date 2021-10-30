bf189649515c0106069c35483960695b19126776
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-10-30 13:05:35 +0300

    Add pdf attachment to LicenseMailer#new_license_email

--- app/mailers/license_mailer.rb
+++ app/mailers/license_mailer.rb
@@ -2,6 +2,18 @@ class LicenseMailer < ApplicationMailer
   def new_license_email
     @user = params[:user]
 
+    attach_license
     mail(to: @user.email, subject: "Your license is ready!")
+  end
+
+  def attach_license
+    pdf = Prawn::Document.new
+    pdf.text(<<~RUBY)
+      Your name: #{@user.name}
+      You email: #{@user.email}
+      Your license number: 1234567890
+    RUBY
+
+    attachments["license.pdf"] = pdf.render
   end
 end
